29ffd1f2afecad13c41e51b6cca7231aaa7f60da
Seivan Heidari <seivan.heidari@icloud.com>
2014-11-29 20:04:10 +0100

    Add tests for testOnSubscribeMock

--- TestsAndSample/Tests/MockingTests.swift
+++ TestsAndSample/Tests/MockingTests.swift
@@ -8,6 +8,9 @@
 
 import XCTest
 
+func ==(lhs:(String, AnyObject?), rhs:(String, AnyObject?)) -> Bool {
+  return lhs.0 == rhs.0 && lhs.1 === rhs.1
+}
 class MockingTests: XCTestCase {
 
   var hub = NotificationHub<String>()
@@ -33,7 +36,14 @@ class MockingTests: XCTestCase {
     let second = self.hub.subscribeNotificationForName("the second name", sender: nil) { n in }
     
     XCTAssertEqual(mocks.count, 2)
-    
+    XCTAssertEqual(mocks.first!.0, first.name)
+    XCTAssertTrue(mocks.first!.1 === first.sender)
+    XCTAssertTrue(mocks.first!.1 === self)
+
+    XCTAssertEqual(mocks.last!.0, second.name)
+    XCTAssertTrue(mocks.last!.1 === second.sender)
+    XCTAssertNil(mocks.last!.1)
+
     
   }
 
