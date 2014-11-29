194234f149bd0317c608d6ab86f0eb87669bff00
Seivan Heidari <seivan.heidari@icloud.com>
2014-11-29 21:00:05 +0100

    Finished mockingTests.

--- TestsAndSample/Tests/MockingTests.swift
+++ TestsAndSample/Tests/MockingTests.swift
@@ -26,7 +26,7 @@ class MockingTests: XCTestCase {
     NotificationHubDefault.removeAllNotifications()
   }
   
-  func testOnSubscribeMock() {
+  func testOnSubscribingMock() {
     var mocks = [(String, AnyObject?)]()
     NotificationHubMock.onSubscribingMock { (name, sender) in
       mocks.append((name, sender))
@@ -43,9 +43,67 @@ class MockingTests: XCTestCase {
     XCTAssertEqual(mocks.last!.0, second.name)
     XCTAssertTrue(mocks.last!.1 === second.sender)
     XCTAssertNil(mocks.last!.1)
+  }
+  
+  func testOnPublishingMock() {
+    var mocks = [(String, AnyObject?, Any?)]()
+    NotificationHubMock.onPublishingMockHandler { (name, sender, userInfo) in
+      mocks.append((name, sender, userInfo))
+    }
+    
+    let first = NotificationHubDefault.subscribeNotificationForName("A name", sender: self) { n in }
+    let second = self.hub.subscribeNotificationForName("the second name", sender: nil) { n in }
+    
+    NotificationHubDefault.publishNotification(first, userInfo: nil)
+    self.hub.publishNotification(second, userInfo: second.name)
+    NotificationHubDefault.publishNotificationName("A name", sender: nil)
+    
+    XCTAssertEqual(mocks.count, 2)
+    XCTAssertEqual(mocks.first!.0, first.name)
+    XCTAssertTrue(mocks.first!.1 === first.sender)
+    XCTAssertTrue(mocks.first!.1 === self)
+    XCTAssertTrue(mocks.first!.2 == nil)
+    
+    XCTAssertEqual(mocks.last!.0, second.name)
+    XCTAssertTrue(mocks.last!.1 === second.sender)
+    XCTAssertNil(mocks.last!.1)
+    XCTAssertEqual((mocks.last!.2 as String), second.name)
+
+  }
+  
+  
+  func testOnRemovingMock() {
+    var mocks = [(String, AnyObject?)]()
+    NotificationHubMock.onRemovingMockHandler { (name, sender) in
+      mocks.append((name, sender))
+    }
+    NotificationHubMock.onPublishingMockHandler { (name, sender, userInfo) in
+      mocks.append((name, sender))
+    }
+
+    let first = NotificationHubDefault.subscribeNotificationForName("A name", sender: self) { n in }
+    let second = self.hub.subscribeNotificationForName("the second name", sender: nil) { n in }
+    
+    
+    NotificationHubDefault.removeNotification(first)
+    self.hub.removeNotification(second)
+    
+    NotificationHubDefault.publishNotification(first, userInfo: nil)
+    self.hub.publishNotification(second, userInfo: second.name)
+    NotificationHubDefault.publishNotificationName("A name", sender: nil)
+    
+    XCTAssertEqual(mocks.count, 2)
+    XCTAssertEqual(mocks.first!.0, first.name)
+    XCTAssertTrue(mocks.first!.1 === first.sender)
+    XCTAssertTrue(mocks.first!.1 === self)
 
     
+    XCTAssertEqual(mocks.last!.0, second.name)
+    XCTAssertTrue(mocks.last!.1 === second.sender)
+    XCTAssertNil(mocks.last!.1)
+    
   }
+
 
 }
   
